a148d20d8adc181a0fedcb8f86a264578bb91515
Mark hanley <home@markhanley.co.uk>
2023-05-20 09:56:14 +0100

    Complete up to challenge 4

--- src/model/products.js
+++ src/model/products.js
@@ -1,1 +1,46 @@
 const db = require("../database/db.js");
+
+const get_all_products = db.prepare(/*sql*/
+    `
+    SELECT
+        id,
+        name,
+        quantity_per_unit,
+        unit_price,
+        units_in_stock,
+        units_on_order
+    FROM
+        products
+    `);
+
+const  listProducts = () => {
+    return get_all_products.all();
+}
+
+const find_products = db.prepare(/*sql*/`
+    SELECT id, name
+    FROM products
+    WHERE name LIKE $search_string`);
+
+const searchProducts = (searchString) => {
+    return find_products.all({search_string: `%${searchString}%`});
+};
+
+const find_product = db.prepare(/*sql*/`
+    SELECT
+        prod.id,
+        prod.name,
+        cat.name AS category_name,
+        cat.description AS category_description
+    FROM products AS prod
+    JOIN categories AS cat
+    ON prod.category_id = cat.id
+    WHERE prod.id = ?`);
+
+const getProduct = (id) => {
+    return find_product.get(id);
+};
+
+
+
+module.exports = {listProducts, searchProducts, getProduct};
